4be217cae382120a873715338d75144e33d4de97
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-02-25 09:06:59 +0530

    Http module json return

--- httpModule/main.js
+++ httpModule/main.js
@@ -1,17 +1,38 @@
 import http from "http"
+import { promises } from "fs"
 
 const PORT = 8080
 
-let db = []
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     if (req.url === "/show" && req.method === "POST") {
         res.setHeader("Content-Type", "application/json")
         res.statusCode = 200
         res.end(JSON.stringify({
-            body: "Kill yourself"
+            body: "Hello gaiz"
         }))
         return -1;
+    }
+    if (req.url === "/get_json" && req.method === "GET") {
+        res.setHeader("Content-Type", "application/json")
+        try {
+            const data = await promises.readFile("obj.json", { encoding: "utf8" })
+            res.end(data)
+            return 0
+        } catch {
+            res.end()
+            return 0
+        }
+    }
+    if (req.url === "/get_html" && req.method === "GET") {
+        res.setHeader("Content-Type", "text/html")
+        try {
+            const data = await promises.readFile("ssr.html", { encoding: "utf8" })
+            res.end(data)
+            return 0
+        } catch {
+            res.end()
+            return 0
+        }
     }
     res.setHeader("Content-Type", "text/html")
     res.write("<body style='background:red'> Hello </h1>")
